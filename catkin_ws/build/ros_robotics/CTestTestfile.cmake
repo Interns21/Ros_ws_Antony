57afa16da163b808475071371677423d29df74b8
Antony <121901004@smail.iitpkd.ac.in>
2021-07-08 21:00:46 +0530

    first commit

---
+++ catkin_ws/build/ros_robotics/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/antonykkk/catkin_ws/src/ros_robotics
+# Build directory: /home/antonykkk/catkin_ws/build/ros_robotics
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
